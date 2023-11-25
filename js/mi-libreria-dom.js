74de718e9323b55c06b116de3559dc7fd2a51fa0
iber noe <97256676+iberNoe@users.noreply.github.com>
2023-11-24 20:47:20 -0600

    Update mi-libreria-dom.js

--- js/mi-libreria-dom.js
+++ js/mi-libreria-dom.js
@@ -23,4 +23,29 @@ const MiLibreriaDOM = {
     },
 
     // Otras funciones que desees agregar...
+    import { addStyles } from './styles';  // Ajusta la ruta según la estructura de tus archivos
+
+const MiLibreriaDOM = {
+    createElement: (tagName, attributes = {}, content = "") => {
+        const element = document.createElement(tagName);
+        for (const key in attributes) {
+            if (attributes.hasOwnProperty(key)) {
+                element.setAttribute(key, attributes[key]);
+            }
+        }
+        element.innerHTML = content;
+        return element;
+    },
+
+    appendTo: (element, target) => {
+        target.appendChild(element);
+    },
+
+    addStyles,  // Importa y agrega la función addStyles de styles.js
+
+    // Otras funciones que desees agregar...
+};
+
+export default MiLibreriaDOM;
+
 };
